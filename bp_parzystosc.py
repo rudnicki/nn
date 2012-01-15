2344e4131f6c0644f365d6a8ad19f94a7048307b
Mateusz Rudnicki <rudnicki@student.agh.edu.pl>
2012-01-15 21:01:28 +0100

    .

--- bp_parzystosc.py
+++ bp_parzystosc.py
@@ -12,7 +12,7 @@ print "\n<<Initial weights>>"
 NN.show()
 
 #learning the pattern
-NN.learnBP(pattern, classes, iterations = 10000)
+NN.learnBP(pattern, classes, iterations = 5000)
 
 print "\n<<Weights after learning>>"
 NN.show()
@@ -24,4 +24,4 @@ print "input_vector", "---->", "output_vector", "---->", "winner id"
 for x in pattern:
     output = NN.output(x)
     max_idx, max_val = max(enumerate(output), key=operator.itemgetter(1))
-    print short(x), "---->", short(NN.out()), "---->", max_idx
+    print short(x), "---->", short(NN.out()), "---->", abs(round(max_val))
